66173615f203ddb77bd865335d2dfba09249d0e9
Mark Entingh <entingh@gmail.com>
2019-12-27 18:32:05 -0800

    can reset cache for local machine

---
+++ ResetCache.cs
@@ -0,0 +1,18 @@
+﻿using Saber.Common.ProcessInfo;
+
+namespace Saber.Vendor.ImportExport
+{
+    public class ResetCache : Controller, IVendorController
+    {
+        public override string Render(string body = "")
+        {
+            Server.Cache.Clear();
+            ViewCache.cache.Clear();
+            Gulp.Task("default");
+            Gulp.Task("default:website");
+            return "";
+        }
+
+        
+    }
+}
---
+++ ViewRenderer.cs
@@ -0,0 +1,13 @@
+﻿namespace Saber.Vendor.ResetCache
+{
+    [ViewPath("/Views/AppSettings/appsettings.html")]
+    public class ViewRenderer : IVendorViewRenderer
+    {
+        public void Render(Request request, View view)
+        {
+            var settingsView = new View("/Vendor/ResetCache/resetcache.html");
+            request.AddScript("/js/vendor/resetcache/resetcache.js");
+            view["vendor"] += settingsView.Render();
+        }
+    }
+}
