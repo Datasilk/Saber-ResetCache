622a5eecf8308bcca9bf4c74f7689e113fc059c0
Mark Entingh <entingh@gmail.com>
2020-11-13 14:33:19 -0800

    added publish process & updated README.md

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,44 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    sevenBin = require('7zip-bin'),
+    sevenZip = require('node-7z');
 
-var release = 'bin/Release/netcoreapp3.1/';
-var publish = 'bin/Publish/ResetCache/'
+var app = 'ResetCache';
+var release = 'bin/Release/net5.0/';
+var publish = 'bin/Publish/';
 
-gulp.task('publish', function () {
-    p = gulp.src(['resetcache.html', 'resetcache.js', release + 'Saber.Vendor.ResetCache.dll'])
-        .pipe(gulp.dest(publish, { overwrite: true }));
-    return p;
+function publishToPlatform(platform) {
+    return gulp.src([
+        //include custom resources
+        'resetcache.html', 'resetcache.js',
+        //include all files from published folder
+        release + platform + '/publish/*',
+        //exclude unwanted dependencies
+        '!' + release + platform + '/publish/Core.dll',
+        '!' + release + platform + '/publish/Saber.Core.dll',
+        '!' + release + platform + '/publish/Saber.Vendor.dll',
+        '!' + release + platform + '/publish/*.deps.json'
+    ]).pipe(gulp.dest(publish + '/' + platform + '/' + app, { overwrite: true }));
+}
+
+gulp.task('publish:win-x64', () => {
+    return publishToPlatform('win-x64');
 });
+
+gulp.task('publish:linux-x64', () => {
+    return publishToPlatform('linux-x64');
+});
+
+gulp.task('zip', () => {
+    setTimeout(() => {
+        //wait 500ms before creating zip to ensure no files are locked
+        process.chdir(publish);
+        sevenZip.add(app + '.7z', app, {
+            $bin: sevenBin.path7za,
+            recursive: true
+        });
+        process.chdir('../..');
+    }, 500);
+    return gulp.src('.');
+});
+
+gulp.task('publish', gulp.series('publish:win-x64', 'publish:linux-x64', 'zip'));
