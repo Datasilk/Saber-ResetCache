403db393f9fa9eeced84a0a76c381b344f0f667e
Mark Entingh <entingh@gmail.com>
2021-01-06 23:10:07 -0800

    added Icon to IVendorInfo

--- Info.cs
+++ Info.cs
@@ -7,6 +7,7 @@ namespace Saber.Vendors.ResetCache
         public string Key { get; set; } = "ResetCache";
         public string Name { get; set; } = "Reset Cache";
         public string Description { get; set; } = "Allows administrators to clear all in-memory cached objects related to web pages, such as HTML files, multi-lingual content JSON files, and page settings JSON files.";
+        public string Icon { get; set; }
         public Version Version { get; set; } = "1.0.0.0";
     }
 }
