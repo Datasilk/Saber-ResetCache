28866e4d69feb932fc663e182162b3df0fe1369d
Mark Entingh <entingh@gmail.com>
2023-01-05 00:27:37 -0600

    fixed ambiguious name\

--- Info.cs
+++ Info.cs
@@ -8,6 +8,6 @@ namespace Saber.Vendors.ResetCache
         public string Name { get; set; } = "Reset Cache";
         public string Description { get; set; } = "Allows administrators to clear all in-memory cached objects related to web pages, such as HTML files, multi-lingual content JSON files, and page settings JSON files.";
         public string Icon { get; set; }
-        public Version Version { get; set; } = "1.0.0.0";
+        public Vendor.Version Version { get; set; } = "1.0.0.0";
     }
 }
